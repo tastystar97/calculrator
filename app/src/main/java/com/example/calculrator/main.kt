e354d673aa9707f5d63f9102adfef311d4a745aa
tastystar97 <rader159>
2023-07-18 18:22:57 +0900

    클래스추가함

--- app/src/main/java/com/example/calculrator/main.kt
+++ app/src/main/java/com/example/calculrator/main.kt
@@ -1,24 +1,29 @@
+import com.example.calculrator.AddOperation
+import com.example.calculrator.SubtractOperation
+import com.example.calculrator.MultiplyOperation
+import com.example.calculrator.DivideOperation
+
 fun main() {
-    var num1: Double
-    var num2: Double
+    var num1: Int
+    var num2: Int
     var operator: Char
     var result: Double
 
     println("숫자를 입력하세요")
-    num1 = readLine()?.toDoubleOrNull() ?: 0.0
+    num1 = readLine()?.toIntOrNull() ?: 0
 
     println("연산자를 입력하세요 (+, -, *, / 중 하나)")
     operator = readLine()?.getOrNull(0) ?: '+'
 
     println("두 번째 숫자를 입력하세요")
-    num2 = readLine()?.toDoubleOrNull() ?: 0.0
+    num2 = readLine()?.toIntOrNull() ?: 0
 
     result = when (operator) {
-        '+' -> num1 + num2
-        '-' -> num1 - num2
-        '*' -> num1 * num2
-        '/' -> num1 / num2
-        else -> throw IllegalArgumentException("유효하지 않은 연산자입니다.")
+        '+' -> AddOperation().operate(num1, num2)
+        '-' -> SubtractOperation().operate(num1, num2)
+        '*' -> MultiplyOperation().operate(num1, num2)
+        '/' -> DivideOperation().operate(num1, num2)
+        else -> throw IllegalArgumentException("잘못된 연산자입니다.")
     }
 
     println("결과: $result")
