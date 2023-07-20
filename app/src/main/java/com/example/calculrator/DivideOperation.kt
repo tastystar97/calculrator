00390d72a84fc4c87910573ea8c46146449b4eda
tastystar97 <rader159>
2023-07-20 12:23:17 +0900

    코드 간결화

--- app/src/main/java/com/example/calculrator/DivideOperation.kt
+++ app/src/main/java/com/example/calculrator/DivideOperation.kt
@@ -2,6 +2,6 @@ package com.example.calculrator
 import com.example.calculator.AbstractOperation
 
 class DivideOperation : AbstractOperation() {
-    override fun operate(num1: Double, num2: Double): Double = (num1 / num2).toDouble()
+    override fun operate(num1: Double, num2: Double): Double = (num1 / num2)
 }
 
